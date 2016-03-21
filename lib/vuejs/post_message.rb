23bd9067f105b5895ec323bd2e3a7c0eb1c8240a
ytbryan <ytbryan@gmail.com>
2016-03-21 12:34:58 +0800

    vuejs-1.0.18.gem

--- lib/vuejs/post_message.rb
+++ lib/vuejs/post_message.rb
@@ -1,11 +1,5 @@
 module Log
   MESSAGE = "
-
-  ######################
-  ## vuejs
-  ######################
-
-  Full Changelog -> https://github.com/ytbryan/vuejs/blob/master/changelog.md
   Pull Request ->  https://github.com/ytbryan/vuejs/pulls
   "
 end
