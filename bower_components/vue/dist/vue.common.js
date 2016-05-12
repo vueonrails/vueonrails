2f181a68fea744b81591a03d42282d74cd130040
ytbryan <ytbryan@gmail.com>
2016-05-12 21:07:47 +0800

    added vue.js 1.0.24

--- bower_components/vue/dist/vue.common.js
+++ bower_components/vue/dist/vue.common.js
@@ -1,5 +1,5 @@
 /*!
- * Vue.js v1.0.22
+ * Vue.js v1.0.24
  * (c) 2016 Evan You
  * Released under the MIT License.
  */
@@ -1136,8 +1136,9 @@ function query(el) {
  */
 
 function inDoc(node) {
-  var doc = document.documentElement;
-  var parent = node && node.parentNode;
+  if (!node) return false;
+  var doc = node.ownerDocument.documentElement;
+  var parent = node.parentNode;
   return doc === node || doc === parent || !!(parent && parent.nodeType === 1 && doc.contains(parent));
 }
 
@@ -3011,19 +3012,26 @@ function resetBatcherState() {
  */
 
 function flushBatcherQueue() {
-  runBatcherQueue(queue);
-  queue.length = 0;
-  runBatcherQueue(userQueue);
-  // user watchers triggered more internal watchers
-  if (queue.length) {
+  var _again = true;
+
+  _function: while (_again) {
+    _again = false;
+
     runBatcherQueue(queue);
+    runBatcherQueue(userQueue);
+    // user watchers triggered more watchers,
+    // keep flushing until it depletes
+    if (queue.length) {
+      _again = true;
+      continue _function;
+    }
+    // dev tool hook
+    /* istanbul ignore if */
+    if (devtools && config.devtools) {
+      devtools.emit('flush');
+    }
+    resetBatcherState();
   }
-  // dev tool hook
-  /* istanbul ignore if */
-  if (devtools && config.devtools) {
-    devtools.emit('flush');
-  }
-  resetBatcherState();
 }
 
 /**
@@ -3049,6 +3057,7 @@ function runBatcherQueue(queue) {
       }
     }
   }
+  queue.length = 0;
 }
 
 /**
@@ -7768,7 +7777,7 @@ function extractFragment(nodes, parent) {
     var node = nodes[i];
     if (isTemplate(node) && !node.hasAttribute('v-if') && !node.hasAttribute('v-for')) {
       parent.removeChild(node);
-      node = parseTemplate(node);
+      node = parseTemplate(node, true);
     }
     frag.appendChild(node);
   }
@@ -10004,7 +10013,7 @@ function installGlobalAPI (Vue) {
 
 installGlobalAPI(Vue);
 
-Vue.version = '1.0.22';
+Vue.version = '1.0.24';
 
 // devtools global hook
 /* istanbul ignore next */
