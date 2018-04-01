ab6b399f902f94f955d623abcb903571a0af1904
ytbryan <ytbryan@gmail.com>
2018-04-02 06:44:27 +0800

    ✍️ vue_generator and pack.js

--- lib/generators/vue/vue_generator.rb
+++ lib/generators/vue/vue_generator.rb
@@ -1,20 +1,38 @@
 class VueGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../../generator_templates', __FILE__)
+
+  argument :name, :type => :string, :default => :index 
+  class_option :single, type: :boolean, default: false
+  class_option :vuex, type: :boolean, default: false
+  # class_option :helpers, type: :boolean, default: false
+
   PACKS_PATH = "app/javascript/packs"
   PARTS_PATH = "app/javascript/parts"
 
-  source_root File.expand_path('../../generator_templates', __FILE__)
-  argument :name, :type => :string, :default => :index 
-  class_option :single, type: :boolean, default: false
-
   def vue 
-    if options[:single] == false
-      create_component_with_seperate_concern_using(name)
+    if options[:single] 
+      create_single_file_component_using(name) 
     else 
-      create_single_file_component_using(name)
+      create_component_with_seperate_concern_using(name)
     end
+      
+    options[:vuex] ? add_vuex_to_component(name) : nil
+    options[:helpers] ? add_helpers_to_component(name) : nil
   end
 
   private
+  def add_helpers_to_component name
+    puts "adding helpers"
+  end
+
+  def add_vuex_to_component name
+    puts "adding vuex"
+    # yarn add vuex
+    # import vuex from 'vuex'
+    # Vue.use(vuex)
+    # Generate a vuex.html.erb
+  end
+
   def create_component_with_seperate_concern_using name
     template "pack.js", "#{PACKS_PATH}/#{name}.js"      
     template "index.vue", "#{PARTS_PATH}/#{name}/#{name}.vue"    
