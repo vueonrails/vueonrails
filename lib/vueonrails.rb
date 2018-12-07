4505920357344eddd8232b5fbb089366fe3b5683
m5o <m5o@gmx.com>
2018-12-07 15:04:12 +0100

    remove unused thor library (#23)
    * remove unused thor library

--- lib/vueonrails.rb
+++ lib/vueonrails.rb
@@ -1,13 +1,7 @@
 require "vueonrails/version"
-require "thor"
 
 module VueOnRails
   class Engine < ::Rails::Engine
     engine_name 'vueonrails'
-  end
-
-  class Base < Thor
-    check_unknown_options!
-    package_name 'vueonrails'
   end
 end
