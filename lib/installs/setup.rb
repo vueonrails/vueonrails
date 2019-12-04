71467dfe12ff5802248f603b6079304e2c8dcd56
pokvalitov <pokvalitov@energis.tech>
2019-12-04 15:24:48 +0500

    fix file name in the setup script (#33)

--- lib/installs/setup.rb
+++ lib/installs/setup.rb
@@ -90,8 +90,8 @@ insert_into_file Rails.root.join("package.json").to_s,
 scripts, after: "\"private\": true,\n"
 
 # Insert locale.js as a default i18n and add second locale cn.yml
-copy_file "#{__dir__}/../generators/templates/i18n/index.js", Rails.root.join("app/javascript/locales/locale.js").to_s
-copy_file "#{__dir__}/../generators/templates/i18n/cn.yml", Rails.root.join("config/locales/cn.yml").to_s
+copy_file "#{__dir__}/../generators/templates/I18n/index.js", Rails.root.join("app/javascript/locales/locale.js").to_s
+copy_file "#{__dir__}/../generators/templates/I18n/cn.yml", Rails.root.join("config/locales/cn.yml").to_s
 
 # Insert store.js as a simple store for components' state 
 copy_file "#{__dir__}/../generators/templates/stores/index.js", Rails.root.join("app/javascript/parts/store.js").to_s
